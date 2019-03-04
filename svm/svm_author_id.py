ef9857537534497bcb91de41f448f42dbeb8753c
PSurya9195 <38082208+PSurya9195@users.noreply.github.com>
2019-03-05 03:17:06 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,9 +22,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
+
 #########################################################
 ### your code goes here ###
-
+clf=svc(kernel='linear')
+clf.fit(features_train, features_test)
+pred=clf.predict(labels_train)
+     
 #########################################################
 
 
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+def submitAccuracy():
+    return acc
